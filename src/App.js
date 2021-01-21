b8d935ca02b741cb4e5d35aa762890ada5d3d37e
Rafael Sousa Herves <rsherves@gmail.com>
2021-01-21 18:50:30 +0100

    React logo with scientist illustration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
+import scientist from './img/female-scientist-illustration.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Page in construction.
-        </p>
+        <p>Page under construction.</p>
+        <img src={scientist} className="sci-illustration" alt="female scientist illustration" />
+        <span className="img-credits">Image credits <a href="https://www.freepik.com/vectors/hand">here</a></span>
       </header>
     </div>
   );
