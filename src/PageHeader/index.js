451b43b6e00ce6f080575a2ee50bfaf146006de7
Rafael Sousa Herves <rsherves@gmail.com>
2021-02-11 16:24:03 +0100

    Initial Header implementation

--- src/PageHeader/index.js
+++ src/PageHeader/index.js
@@ -1,9 +1,30 @@
 import './index.css';
 
+function MenuEntry(props) {
+  return (
+    <div className="menu-entry">
+      <div className="entry-button">
+        {props.value}
+      </div>
+      <div className="entry-selection"></div>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div className="flex-container">
+      <MenuEntry value="About me" />
+      <MenuEntry value="Research" />
+      <MenuEntry value="Publications" />
+    </div>
+  );
+}
+
 function PageHeader() {
   return (
     <div className="PageHeader">
-      <p>This is the page Header</p>
+      <Menu />
     </div>
   );
 }
