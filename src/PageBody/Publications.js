b2c1a12f180240b421e312f0b1d7d09c980b036d
Rafael Sousa Herves <rsherves@gmail.com>
2021-02-13 20:12:46 +0100

    Sample page for Publications

--- src/PageBody/Publications.js
+++ src/PageBody/Publications.js
@@ -1,11 +1,53 @@
+import React from 'react';
+import PublicationData from './PublicationData.js';
 import './Publications.css';
 
-function Publications() {
+function PublicationTableRow(props) {
   return (
-    <div className="Publications">
-      <p>Publications</p>
-    </div>
+    <tr>
+      <td>
+        <div className="pub-title">{props.data.title}</div>
+        <div className="pub-authors">{props.data.authors}</div>
+        <div className="pub-source">{props.data.source}</div>
+      </td>
+      <td>
+        <div className="pub-date">{props.data.date.getFullYear()}</div>
+      </td>
+    </tr>
   );
+}
+
+class Publications extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      publications: PublicationData().getAllSortedByDate(),
+    };
+  }
+
+  render() {
+    const publications = this.state.publications.map((p, i) => {
+      return (
+        <PublicationTableRow key={i} data={p} />
+      );
+    });
+    return(
+      <div className="Publications">
+        <div className="publications-header">List of articles published</div>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            {publications}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Publications;
