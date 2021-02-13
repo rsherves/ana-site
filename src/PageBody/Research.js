9a32ff2721994ff349868e9fd257e78d3fd50242
Rafael Sousa Herves <rsherves@gmail.com>
2021-02-13 23:46:44 +0100

    Research page with image

--- src/PageBody/Research.js
+++ src/PageBody/Research.js
@@ -1,9 +1,27 @@
 import './Research.css';
+import illustration from '../img/woman-science-technology.jpg';
 
 function Research() {
+  const imgCreditUrl =
+    "https://ec.europa.eu/research/infocentre/article_en.cfm?&artid=51905&caller=other";
   return (
     <div className="Research">
-      <p>Research</p>
+      <div className="img-wrapper">
+        <img
+          src={illustration}
+          className="w-sci-tec-illustration"
+          alt="women in science and technology"
+        />
+      </div>
+      <div className="img-credits">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={imgCreditUrl}
+        >
+          © Naufal #317011810 source: stock.adobe.com 2020
+        </a>
+      </div>
     </div>
   );
 }
