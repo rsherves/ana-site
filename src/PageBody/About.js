e066d2b9518f5144de60cc04afddea79c5a334cf
Rafael Sousa Herves <rsherves@gmail.com>
2021-02-26 23:30:12 +0100

    Set real text in 'About me'

--- src/PageBody/About.js
+++ src/PageBody/About.js
@@ -10,10 +10,10 @@ function About() {
           <img src={anaOffice} className="ana-office" alt="Photo in the office" />
         </div>
         <div className="flex-item-txt">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam interdum porta pellentesque. Praesent porta malesuada orci, in viverra risus sollicitudin vitae.</p>
-          <p>Nunc varius lorem augue. Vivamus et iaculis risus. Phasellus a iaculis massa. Etiam at mauris nisi. Phasellus ac volutpat augue, quis commodo justo. Integer congue id leo accumsan ultrices. Sed eu mattis tortor. Cras posuere hendrerit mi, nec sollicitudin est lacinia vel. Maecenas gravida nunc a magna volutpat euismod.</p>
-          <p>Praesent cursus orci vel arcu finibus luctus. Donec sollicitudin nulla vel dapibus tristique. Duis egestas sodales venenatis. Sed vehicula eget mi eu maximus. Mauris luctus volutpat mattis. Phasellus nisl ipsum, suscipit in molestie ut, ultrices sed augue. Nunc sed pretium tortor, maximus dignissim nulla. Donec tempus dui tristique ante placerat, pretium porta risus vestibulum. Proin pretium lorem id odio gravida, tincidunt finibus orci vehicula.</p>
-          <p>Sed consectetur mi non diam elementum dapibus. In sit amet ultricies velit. Aenean ac tellus id massa dapibus auctor. Sed eu sem finibus, elementum turpis sed, sagittis nisl. Praesent blandit ex vel sem molestie viverra.</p>
+          <p>I received my PhD (Extraordinary Doctorate Award) from the University of Santiago de Compostela in 2011 after working on the preparation of dendritic nanostructures for biomedical applications in the group of Prof. R. Riguera and Prof. E. Fernandez-Megia.</p>
+          <p>In 2012 I was awarded a Marie Curie Intra-European Fellowship to carry out my postdoctoral research at Freie Universität Berlin, under the supervision of Prof. M. Calderón and Prof. R. Haag. My work in Berlin was focused on the preparation of responsive nanogels and polymer-drug conjugates for applications in drug and gene delivery.</p>
+          <p>In 2015 I joined the Glycosystems Laboratory under the supervision of J. Rojo (IIQ, CSIC, Sevilla), where I worked first as a Talent Hub Fellow (Marie Curie COFUND) and then as a Juan de la Cierva Incorporación Researcher preparing glycodendritic nanostructures for their interaction with biologically relevant lectins.</p>
+          <p>Since November 2019 I work as Lecturer in Organic Chemistry (Prof. Ayudante Doctor) at the Faculty of Pharmacy of the University of Castilla-La Mancha. My research interests include the development of light-sensitive nanostructures with applications in biomedicine.</p>
         </div>
       </div>
     </div>
