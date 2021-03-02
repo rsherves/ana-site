9094056d61dbbee404fffe475875f9af96bdf3ea
Rafael Sousa Herves <rsherves@gmail.com>
2021-03-02 11:23:05 +0100

    Rewrite last paragraph About me

--- src/PageBody/About.js
+++ src/PageBody/About.js
@@ -14,7 +14,7 @@ function About() {
           <p>I received my PhD (Extraordinary Doctorate Award) from the University of Santiago de Compostela in 2011 after working on the preparation of dendritic nanostructures for biomedical applications in the group of Prof. R. Riguera and Prof. E. Fernandez-Megia.</p>
           <p>In 2012 I was awarded a Marie Curie Intra-European Fellowship to carry out my postdoctoral research at Freie Universität Berlin, under the supervision of Prof. M. Calderón and Prof. R. Haag. My work in Berlin was focused on the preparation of responsive nanogels and polymer-drug conjugates for applications in drug and gene delivery.</p>
           <p>In 2015 I joined the Glycosystems Laboratory under the supervision of Dr. J. Rojo (IIQ, CSIC, Sevilla), where I worked first as a Talent Hub Fellow (Marie Curie COFUND) and then as a Juan de la Cierva Incorporación Researcher preparing glycodendritic nanostructures for their interaction with biologically relevant lectins.</p>
-          <p>Since November 2019 I work as Lecturer in Organic Chemistry (Prof. Ayudante Doctor) at the Faculty of Pharmacy of the University of Castilla-La Mancha. My research interests include the development of light-sensitive nanostructures with applications in biomedicine.</p>
+          <p>Since November 2019 I work as Lecturer in Organic Chemistry (<em>Prof. Ayudante Doctor</em>) at the Faculty of Pharmacy of the University of Castilla-La Mancha. My research interests as a member of the Pharmaceutical Organic Chemistry group include the development of light-sensitive nanostructures with applications in biomedicine.</p>
 
           <div className="academic-links-wrapper">
             <div className="flex-container row-wrap">
