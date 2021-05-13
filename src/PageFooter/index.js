3fb077fc3d7127d4c47d5ace4366a6ab3c0db697
Rafael Sousa Herves <rsherves@gmail.com>
2021-05-13 13:39:15 +0800

    Write footer text 'developed by'

--- src/PageFooter/index.js
+++ src/PageFooter/index.js
@@ -1,9 +1,21 @@
+import logoLinked_grey50 from '../img/contact/linkedin_grey50_16.png';
 import './index.css';
 
 function PageFooter() {
   return (
     <footer className="PageFooter">
-      <div className="placeholder">Placeholder for contact, legal terms, etc.</div>
+      <div className="placeholder">
+        Web site developed and maintained by Rafael Sousa Hervés,&nbsp;
+        <a 
+          className="rsherves-contact-link"
+          href="https://www.linkedin.com/in/rsherves/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          contact here
+          <img src={logoLinked_grey50} className="linkedin-logo" alt="LinkedIn logo" />
+        </a>
+      </div>
     </footer>
   );
 }
