b148183072a5f33ab09ad16d1ea77d8b260a5676
Rafael Sousa Herves <rsherves@gmail.com>
2021-05-17 15:58:49 +0800

    Make Footer responsive

--- src/PageFooter/index.js
+++ src/PageFooter/index.js
@@ -4,18 +4,23 @@ import './index.css';
 function PageFooter() {
   return (
     <footer className="PageFooter">
-      <div className="placeholder">
-        Web site developed and maintained by Rafael Sousa Hervés,&nbsp;
-        <a 
-          className="rsherves-contact-link"
-          href="https://www.linkedin.com/in/rsherves/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          contact here
-          <img src={logoLinked_grey50} className="linkedin-logo" alt="LinkedIn logo" />
-        </a>
-      </div>
+      <div className="footer-content-wrapper">
+        <div className="developer-credits">
+          <span>Web site developed and maintained by </span>
+          <span className="ws-nowrap">Rafael Sousa Hervés,</span>
+          <span> </span> {/* on purpose wrappable white space */}
+          <span className="ws-nowrap">
+            <a
+              href="https://www.linkedin.com/in/rsherves/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              contact here
+              <img src={logoLinked_grey50} className="linkedin-logo" alt="LinkedIn logo" />
+            </a>
+          </span>
+        </div>
+      </div>        
     </footer>
   );
 }
